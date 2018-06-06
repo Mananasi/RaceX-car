8925025837e864700814eef21ffb837749ae86a9
Mananasi <pet-er@live.nl>
2018-06-06 14:38:05 +0200

    Initial Commit

---
+++ Main.ino
@@ -0,0 +1,88 @@
+#include <Servo.h>
+#define HALL 12
+
+#define LINE_START 2
+#define LINE_END 5
+#define LINE_SIZE 4
+
+#define TILT 7
+
+#define SERVO 10
+
+Servo steer;
+
+bool skip = false; //KEEP THIS AT FALSE
+
+bool prevLineSensor[LINE_SIZE];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(HALL, INPUT);
+  pinMode(TILT, INPUT);
+  for(int i = LINE_START; i <= LINE_END; i++)
+    pinMode(i, INPUT);
+    
+  steer.attach(SERVO);
+}
+
+void loop() {
+  // read sensors:
+  bool hallSensor = readHallSensor();
+  bool lineSensor[LINE_SIZE];
+  readLineSensor(lineSensor);
+  bool tiltSensor = readTiltSensor();
+
+  
+  int angle = getServoDegrees(lineSensor);
+  //Serial.println(angle);
+
+  if(!arraysEqual(lineSensor, prevLineSensor)) {
+    if(skip) {
+      skip = false;
+    } else {
+      skip = true;
+      steer.write(angle);
+      Serial.println("Yee");
+    }
+  }
+  
+  //analogWrite(SERVO, angle);
+  
+  delay(10);
+
+  copyArray(lineSensor, prevLineSensor);
+}
+
+
+bool readHallSensor() {
+  return digitalRead(HALL) == 0 ? true : false;
+}
+
+void readLineSensor(bool* input) {
+  for(int i = 0; i < LINE_SIZE; i++)
+    input[i] = digitalRead(i + LINE_START) == 0 ? true : false;
+}
+
+bool readTiltSensor() {
+  return digitalRead(TILT) == 0 ? true : false;
+}
+
+int getServoDegrees(bool lineSensor[]) {
+  //80: rechtdoor, 30: links, 110: rechts
+
+  int rightSensors = 0;
+  for(int i = 0; i < LINE_SIZE/2-1; i++) {
+    if(lineSensor[i]) rightSensors++;
+  }
+
+  int leftSensors = 0;
+  for(int i = LINE_SIZE - 1; i > LINE_SIZE/2; i--) {
+    if(lineSensor[i]) leftSensors++;
+  }
+
+  if(leftSensors > rightSensors) return 30;
+  else if(rightSensors > leftSensors) return 110;
+  else return 80;
+}
+
---
+++ arrayHelper.ino
@@ -0,0 +1,22 @@
+bool arraysEqual(bool arr1[], bool arr2[]) {
+  int size1 = sizeof(arr1)/sizeof(bool);
+  int size2 = sizeof(arr2)/sizeof(bool);
+  if(size1 != size2) return false;
+
+  int equalCount = 0;
+  for(int i = 0; i < size1; i++) {
+    if(arr1[i] == arr2[i]) equalCount++;
+  }
+
+  return equalCount == size1;
+}
+
+void copyArray(bool arrFrom[], bool* arrTo) {
+  int arrSize = sizeof(arrTo) / sizeof(bool);
+
+  for(int i = 0; i < arrSize; i++) {
+    arrTo[i] = arrFrom[i];
+  }
+  
+}
+
